782dfca71fb57c6a60f31786062b3b8b8051e572
wuxiaofang <wuxiaofang@yongche.com>
2016-09-21 14:23:57 +0800

    Initial Commit

---
+++ SubModuleTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SubModuleTest
+//
+//  Created by yongche_w on 16/9/21.
+//  Copyright © 2016年 yidao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SubModuleTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SubModuleTest
+//
+//  Created by yongche_w on 16/9/21.
+//  Copyright © 2016年 yidao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
